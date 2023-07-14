7c05c68657acdd46bd31e0b8ee48dd7dd1e1851a
Uliana Savitskaua <uljasha.sav@gmail.com>
2023-07-14 19:50:11 +0600

    refactor: delete logs

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -19,14 +19,6 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         schema: schema,
         source: queryStr, // строка на языке GraphQL, представляет собой запрашиваемую инфу
       });
-
-      // TODO: перенеси то, что ниже в schema, там сейчас есть подобное, но возвращается null (скорее всего из-за неправильного юза промисов)
-      const data2 = await fastify.prisma.memberType.findUnique({
-        where: {
-          id: "basic",
-        }
-      }).then((res) => { return res });
-      console.log('HERE HAVE DATA (member by id) -----> ', data2);
       return {...res};
     }
   });
